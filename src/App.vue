21a9e16be06b5a1e8d7ba7076119dc10fd0b9d2d
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2026-06-10 19:26:29 +0200

    Change the name of HelloWorld component to PlayingTable, update imports

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import PlayingTable from "./components/PlayingTable.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <PlayingTable />
 </template>
